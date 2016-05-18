f56b6c87e01c231e7143863b28ed0aa9f1e53ef4
zdjohn001 <zdjohn_001@163.com>
2016-05-18 17:32:08 +0800

    Update Program.cs

--- Trains/Program.cs
+++ Trains/Program.cs
@@ -1,17 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
-using Trains.Model;
-using Trains.Controller;
 
 namespace Trains
 {    
     /// <summary>  
     /// ClassName:Program  
-    /// Version:1.0  
-    /// Date:2016/05/10  
+    /// Version:2.0  
+    /// Date:2016/05/17  
     /// Author:Dong Zhao  
     /// </summary>  
     /// <remarks>  
@@ -32,52 +30,14 @@ namespace Trains
         {
             try
             {
-                ////Initialize routeControls instance
-                //routeControls = new RouteControls();
-                ////Set route string, should be match fromTown+toTown+distance of each route, use "," to split each route.
-                //string routeStr = "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7";
-                //if (routeControls.CreateRouteGraphs(routeStr.ToUpper()))
-                //{
-                //    Console.WriteLine("1. Start get the distance of the route A-B-C.");
-                //    Console.WriteLine("Output #1: {0}.", HandleNoRoute(routeControls.GetRouteDistance("ABC")));
-                //    Console.WriteLine();
-                //    Console.WriteLine("2. Start get the distance of the route A-D.");
-                //    Console.WriteLine("Output #2: {0}.", HandleNoRoute(routeControls.GetRouteDistance("AD")));
-                //    Console.WriteLine();
-                //    Console.WriteLine("3. Start get the distance of the route A-D-C.");
-                //    Console.WriteLine("Output #3: {0}.", HandleNoRoute(routeControls.GetRouteDistance("ADC")));
-                //    Console.WriteLine();
-                //    Console.WriteLine("4. Start get the distance of the route A-E-B-C-D.");
-                //    Console.WriteLine("Output #4: {0}.", HandleNoRoute(routeControls.GetRouteDistance("AEBCD")));
-                //    Console.WriteLine();
-                //    Console.WriteLine("5. Start get the distance of the route A-E-D.");
-                //    Console.WriteLine("Output #5: {0}.", HandleNoRoute(routeControls.GetRouteDistance("AED")));
-                //    Console.WriteLine();
-                //    Console.WriteLine("6. Start get the number of trips starting at C and ending at C with a maximum of 3 stops.");
-                //    Console.WriteLine("Output #6: {0}.", HandleNoRoute(routeControls.GetRouteWithMaxStops('C', 'C', 3, "", 0, 0)));
-                //    Console.WriteLine();
-                //    Console.WriteLine("7. Start get the number of trips starting at A and ending at C with exactly 4 stops.");
-                //    Console.WriteLine("Output #7: {0}.", HandleNoRoute(routeControls.GetRoutesWithExactlyStops('A', 'C', 4, "", 0, 0)));
-                //    Console.WriteLine();
-                //    Console.WriteLine("8. Start get the length of the shortest route (in terms of distance to travel) from A to C.");
-                //    Console.WriteLine("Output #8: {0}.", HandleNoRoute(routeControls.GetLengthOfShortestRoute('A', 'C', 0, "", 0)));
-                //    Console.WriteLine();
-                //    Console.WriteLine("9. Start get the length of the shortest route (in terms of distance to travel) from B to B.");
-                //    Console.WriteLine("Output #9: {0}.", HandleNoRoute(routeControls.GetLengthOfShortestRoute('B', 'B', 0, "", 0)));
-                //    Console.WriteLine();
-                //    Console.WriteLine("10. Start get the number of different routes from C to C with a distance of less than 30.");
-                //    Console.WriteLine("Output #10: {0}.", HandleNoRoute(routeControls.GetNumberOfRoutesLimitDistance('C', 'C', 30, "", 0, 0)));
-                //    Console.WriteLine();
-                //}
-
-                TownsModel towns = new TownsModel();
-                DirectRouteController directRoute = RouteFindFactory.CreateRouteControl(FindRouteType.DirectRoute);
-                DirectRouteController routeWithMaxStops = RouteFindFactory.CreateRouteControl(FindRouteType.RouteWithMaxStops);
-                DirectRouteController routeWithExactlyStops = RouteFindFactory.CreateRouteControl(FindRouteType.RouteWithExactlyStops);
-                DirectRouteController lengthOfShortestRoute = RouteFindFactory.CreateRouteControl(FindRouteType.LengthOfShortestRoute);
-                DirectRouteController numberOfRoutesLimitDistance = RouteFindFactory.CreateRouteControl(FindRouteType.NumberOfRoutesLimitDistance);
                 //Initialize routeControls instance
-                //routeControls = new RouteControls();
+                TownsModel towns = new TownsModel();
+                IGetMatchRoutes directRoute = RouteFindFactory.CreateRouteControl(FindRouteType.DirectRoute);
+                IGetMatchRoutes routeWithMaxStops = RouteFindFactory.CreateRouteControl(FindRouteType.RouteWithMaxStops);
+                IGetMatchRoutes routeWithExactlyStops = RouteFindFactory.CreateRouteControl(FindRouteType.RouteWithExactlyStops);
+                IGetMatchRoutes lengthOfShortestRoute = RouteFindFactory.CreateRouteControl(FindRouteType.LengthOfShortestRoute);
+                IGetMatchRoutes numberOfRoutesLimitDistance = RouteFindFactory.CreateRouteControl(FindRouteType.NumberOfRoutesLimitDistance);
+                
                 //Set route string, should be match fromTown+toTown+distance of each route, use "," to split each route.
                 string routeStr = "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7";
                 if (towns.CreateRouteGraphs(routeStr.ToUpper()))
