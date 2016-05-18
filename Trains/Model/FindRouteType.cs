29dd11b0950d8be98cc13c73032d9014cac3d9e5
zdjohn001 <zdjohn_001@163.com>
2016-05-18 17:31:21 +0800

    Update FindRouteType.cs

--- Trains/Model/FindRouteType.cs
+++ Trains/Model/FindRouteType.cs
@@ -1,10 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Trains.Model
+namespace Trains
 {
+    /// <summary>  
+    /// EnumName:FindRouteType  
+    /// Version:1.0  
+    /// Date:2016/05/17  
+    /// Author:Dong Zhao  
+    /// </summary>  
+    /// <remarks>  
+    /// This enum used to determine the find route types.  
+    /// </remarks> 
     public enum FindRouteType 
     {
         DirectRoute,
